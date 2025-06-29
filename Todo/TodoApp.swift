17c1d632559fcfb338dc3b6b1ce0c2be9a130571
Igor Chabanchuk <i.chabanchuk@cdek.ru>
2025-06-29 17:56:50 +0300

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Чабанчук Игорь Романович on 29.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
